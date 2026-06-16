2986f6f0c08af65d60b10ff8b960a8c0556125d5
eab3213 <eab3213@uncw.edu>
2026-06-16 16:29:45 -0400

    Added all basic crud API endpoints for review in reviewController

--- src/main/java/com/eugene_boakye/review_service/controller/ReviewController.java
+++ src/main/java/com/eugene_boakye/review_service/controller/ReviewController.java
@@ -1,6 +1,9 @@
 package com.eugene_boakye.review_service.controller;
 import com.eugene_boakye.review_service.model.Review;
 import com.eugene_boakye.review_service.service.ReviewService;
+
+import tools.jackson.databind.util.JSONPObject;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,4 +28,26 @@ public class ReviewController {
         return reviewService.getAllReviews();
 
     }
+    
+    @GetMapping("/{id}")
+    public Review getReview(@PathVariable Long id){
+        return reviewService.getReviewById(id);
+    }
+
+    @PostMapping
+    public Review createReview(@RequestBody Review review){
+        return reviewService.createReview(review);
+
+    }
+
+    @PutMapping("/{id}")
+    public Review updateReview(@PathVariable Long id, @RequestBody Review updatedReview){
+        return reviewService.updateReview(id, updatedReview);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReview(@PathVariable Long id){
+        reviewService.deleteReview(id);
+    }
+
 }
